b5ee9b5a1e8abd996e76f18ae7a15771759e08d9
Jordan Harband <ljharb@gmail.com>
2015-08-26 22:05:38 -0700

    [Tests] Add more tests.

--- test/tests.js
+++ test/tests.js
@@ -3,6 +3,7 @@
 var forEach = require('foreach');
 var has = Object.prototype.hasOwnProperty;
 var assign = require('object.assign');
+var define = require('define-properties');
 
 var testResults = function (t, iterator, expectedResults) {
 	forEach(expectedResults, function (expected) {
@@ -26,6 +27,67 @@ module.exports = function (matchAll, t) {
 		st.end();
 	});
 
+	t.test('ToString-able objects', function (st) {
+		var str = 'aabc';
+		var strObj = { toString: function () { return str; } };
+		var regex = /[ac]/;
+		var expectedResults = [
+			{ value: assign(['a'], { input: str, index: 0 }), done: false },
+			{ value: assign(['a'], { input: str, index: 1 }), done: false },
+			{ value: assign(['c'], { input: str, index: 3 }), done: false },
+			{ value: null, done: true }
+		];
+		testResults(st, matchAll(strObj, regex), expectedResults);
+		st.end();
+	});
+
+	t.test('#flags', function (st) {
+		st.test('without a flags property', function (s2t) {
+			var str = 'aabc';
+			var regex = /[ac]/;
+			if (define.supportsDescriptors) {
+				Object.defineProperty(regex, 'flags', { value: undefined });
+			}
+			s2t.equal(regex.flags, undefined, 'regex has an undefined "flags" property');
+			var expectedResults = [
+				{ value: assign(['a'], { input: str, index: 0 }), done: false },
+				{ value: assign(['a'], { input: str, index: 1 }), done: false },
+				{ value: assign(['c'], { input: str, index: 3 }), done: false },
+				{ value: null, done: true }
+			];
+			testResults(s2t, matchAll(str, regex), expectedResults);
+			s2t.end();
+		});
+
+		st.test('with a static flags property', function (s2t) {
+			var str = 'AaBC';
+			var regex = /[ac]/;
+			define(regex, { flags: 'i' }, { flags: function () { return true; } });
+			s2t.equal(regex.flags, 'i');
+			var expectedResults = [
+				{ value: assign(['A'], { input: str, index: 0 }), done: false },
+				{ value: assign(['a'], { input: str, index: 1 }), done: false },
+				{ value: assign(['C'], { input: str, index: 3 }), done: false },
+				{ value: null, done: true }
+			];
+			testResults(s2t, matchAll(str, regex), expectedResults);
+			s2t.end();
+		});
+
+		st.test('respects flags', function (s2t) {
+			var str = 'A\na\nb\nC';
+			var regex = /^[ac]/im;
+			var expectedResults = [
+				{ value: assign(['A'], { input: str, index: 0 }), done: false },
+				{ value: assign(['a'], { input: str, index: 2 }), done: false },
+				{ value: assign(['C'], { input: str, index: 6 }), done: false },
+				{ value: null, done: true }
+			];
+			testResults(s2t, matchAll(str, regex), expectedResults);
+			s2t.end();
+		});
+	});
+
 	t.test('returns an iterator', function (st) {
 		var str = 'aabc';
 		var iterator = matchAll(str, /[ac]/g);
@@ -41,6 +103,20 @@ module.exports = function (matchAll, t) {
 			{ value: null, done: true }
 		];
 		testResults(st, iterator, expectedResults);
+		st.end();
+	});
+
+	t.test('ignores the "g" flag', function (st) {
+		var str = 'aabc';
+		var iterator = matchAll(str, /[ac]/);
+		var iteratorGlobal = matchAll(str, /[ac]/g);
+		var result = {}, globalResult = {};
+		st.plan(4);
+		while (!result.done && !globalResult.done) {
+			result = iterator.next();
+			globalResult = iteratorGlobal.next();
+			st.deepEqual(result, globalResult, 'results do not differ: ' + JSON.stringify(result) + ', ' + JSON.stringify(globalResult));
+		}
 		st.end();
 	});
 };
